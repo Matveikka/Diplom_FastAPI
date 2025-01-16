0e0d7ef8c390994602894d549aebe21678f501e8
Matveikka <matwei.sklyarenko@yandex.ru>
2025-01-16 16:05:56 +0300

    .

--- Diplom_FastAPI/main.py
+++ Diplom_FastAPI/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routers import post
 
 app = FastAPI()
 app.include_router(post.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
